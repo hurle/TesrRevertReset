ba790d042088305ff4b70759139f4f9aa845abbd
hurle <46908944+hurle@users.noreply.github.com>
2021-02-08 14:42:39 -0600

    Proyecto Nuevo
    Creacion de proyecto

---
+++ RevertCreate/RevertCreate/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RevertCreate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test");
+        }
+    }
+}
